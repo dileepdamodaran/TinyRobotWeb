51cbea51b6e80644514ca4c18a9bdbc6c855e8ce
dileepdamodaran <dileepdamodaran@dileeps-mbp.fios-router.home>
2018-09-02 20:20:28 -0400

    beanname changes

--- TinyRobotWeb/src/com/tw/controller/TinyRobotApp.java
+++ TinyRobotWeb/src/com/tw/controller/TinyRobotApp.java
@@ -1,6 +1,12 @@
 package com.tw.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TinyRobotApp {
@@ -9,6 +15,19 @@ public class TinyRobotApp {
 	    public static void main(String[] args) {
 	        SpringApplication.run(TinyRobotApp.class, args);
 	    }
+	    @Bean
+	    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	        return args -> {
 
+	            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+	            String[] beanNames = ctx.getBeanDefinitionNames();
+	            Arrays.sort(beanNames);
+	            for (String beanName : beanNames) {
+	                System.out.println(beanName);
+	            }
+
+	        };
+	    }
 	
 }
--- TinyRobotWeb/src/com/tw/controller/TinyRobotHomeController.java
+++ TinyRobotWeb/src/com/tw/controller/TinyRobotHomeController.java
@@ -1,11 +1,11 @@
 package com.tw.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TinyRobotHomeController {
 	@GetMapping("/tinyrobot")
 	public String loadHomePage(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,5 +14,3 @@ public class TinyRobotHomeController {
         return "homepage";
     }
 }
-//src/main/java/hello/GreetingController.java
-//src/main/resources/templates/greeting.html
